384be1ca3ff7e38c29a8becd16c76dc573492d30
RZ_cola <168092735+kirkiboi@users.noreply.github.com>
2025-09-06 01:16:23 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -74,7 +74,7 @@ const messagePicker = [
     "YOU GOT THE BRAAAAAIN",
     "may humihingi ng tulong bomba na",
     "puff ka muna!",
-    "proud si malupiton sa imoha yot hahahahha"
+    "proud si malupiton sa imoha yot hahahahha",
     "yot, i really want it to be us, i really want it to be you. as much as i want to tell you this in person, i just cant because im scared you might break. i really hate it when you cry cause i have really have to try and stay composed. im writing this while im at the bus, hoping you'll find this sooner than ever. I cant keep it off my mind so I have to write it down. FOR FUCKS SAKE ALL I WANT IS FOR YOU TO TELL ME YOU NEED ME. TELL ME YOU WANT ME TO STAY AND THAT YOULL CHOOSE ME OVER ANYBODY ELSE. YOT, I AM AND WILL ALWAYS BE YOURS. IF YOU'LL LOSE THE LOVE YOU HAVE, AND, IF EVER, YOU'LL LOOK FOR IT AGAIN, I HOPE YOU'LL GIVE US A CHANCE. I LOVE YOU NOW AND FOREVER."
 ]
 
@@ -104,3 +104,4 @@ function typeMessage(message) {
     }, 100);
 
 }
+
